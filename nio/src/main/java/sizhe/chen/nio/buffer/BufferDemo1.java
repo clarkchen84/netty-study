59322d414cd4927c6009b3c1c7c664b864869991
clarkchen84 <sizhe.chen1@gmail.com>
2022-07-26 20:56:53 +0800

    buffer

--- nio/src/main/java/sizhe/chen/nio/buffer/BufferDemo1.java
+++ nio/src/main/java/sizhe/chen/nio/buffer/BufferDemo1.java
@@ -1,5 +1,11 @@
 package sizhe.chen.nio.buffer;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * @Author: sizhe.chen
  * @Date: Create in 11:11 下午 2022/7/10
@@ -9,4 +15,27 @@ package sizhe.chen.nio.buffer;
  */
 
 public class BufferDemo1 {
+
+
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile("nio-demo-dir/01.txt", "rw" );
+        FileChannel channel = randomAccessFile.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+        int size = channel.read(byteBuffer);
+        System.out.println(size);
+        while (size != -1){
+            byteBuffer.flip();
+            while (byteBuffer.hasRemaining()){
+                System.out.print(String.valueOf((char)byteBuffer.get()));
+            }
+            byteBuffer.clear();
+            size = channel.read(byteBuffer);
+            System.out.println(size);
+
+        }
+        channel.close();
+
+    }
 }
