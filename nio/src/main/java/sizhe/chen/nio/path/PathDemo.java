f3d1a2177417f7040e006f4417ab63e18e1f7ebb
clarkchen84 <sizhe.chen1@gmail.com>
2022-08-28 11:04:51 +0800

    path

---
+++ nio/src/main/java/sizhe/chen/nio/path/PathDemo.java
@@ -0,0 +1,19 @@
+package sizhe.chen.nio.path;
+
+import java.nio.file.Paths;
+
+/**
+ * @Author: sizhe.chen
+ * @Date: Create in 10:54 上午 2022/8/28
+ * @Description:
+ * @Modified:
+ * @Version:
+ */
+
+public class PathDemo {
+    public static void main(String[] args) {
+        // 常见path实例
+        Paths.get("nio-demo-dir/01.txt");
+        //
+    }
+}
